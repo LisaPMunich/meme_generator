b2fa1e3460d1bb06d157710570b434c42fba1253
LisaPMunich <99111208+LisaPMunich@users.noreply.github.com>
2022-05-04 11:36:08 +0200

    Style Navbar

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavImage from '../../images/troll-face.png'
+import './navbar.css'
+
+export default function Navbar(){
+    return(
+        <nav className="nav">
+        <img className="nav-brand--image" src={NavImage} alt="navbar image"/>
+        <h2 className="nav-brand--title">MemeGenerator</h2>
+        <h4 className="nav--subtitle">React Course - Project 3</h4>
+        </nav>
+    )
+}
